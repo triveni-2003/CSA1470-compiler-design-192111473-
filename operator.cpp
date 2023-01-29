dc19440b675ca64ab9adeeb0cc763c42462935b5
triveni-2003 <115334273+triveni-2003@users.noreply.github.com>
2023-01-29 11:57:46 +0530

    Create operator.cpp

---
+++ operator.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	char value;
+	printf("Delete ws");
+	scanf("%c",& value);
+	if((value=='/n')||(value=='{}')||(value=='//')||(value=='/*'))	
+	{
+		printf("no of ws");
+	}
+	else
+	{
+	return 0;
+
+	}
+}  
